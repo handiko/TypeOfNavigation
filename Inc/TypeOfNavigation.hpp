2cfbe0a7d557371767ee174365c3e19542a29d24
handiko <handikogesang@gmail.com>
2020-12-14 14:02:43 +0700

    first commit

---
+++ Inc/TypeOfNavigation.hpp
@@ -0,0 +1,22 @@
+/*
+ * TypeOfNavigation.hpp
+ *
+ *  Created on: Dec 14, 2020
+ *      Author: handiko
+ */
+
+#ifndef INC_TYPEOFNAVIGATION_HPP_
+#define INC_TYPEOFNAVIGATION_HPP_
+
+#define NAV_DEFAULT 0
+#define NAV_GPS 1
+#define NAV_GLONASS 2
+#define NAV_GPS_GLONASS 3
+#define NAV_LORANC 4
+#define NAV_CHAYKA 5
+#define NAV_INTEGRATED_NAV 6
+#define NAV_SURVEYED 7
+#define NAV_GALILEO 8
+#define NAV_INTERNAL_GNSS 15
+
+#endif /* INC_TYPEOFNAVIGATION_HPP_ */
